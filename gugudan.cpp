bf623fb979feeb3b7fd7d9c4925b1a5976800c14
SH <gkstjgms34@naver.com>
2020-10-16 10:42:56 +0900

    changed code

--- gugudan.cpp
+++ gugudan.cpp
@@ -3,7 +3,7 @@
 using namespace std;
 
 int main(){
-    for(int i = 1; i < 9; i++ ){
+    for(int i = 1; i < 10; i++ ){
         cout << i << "단 시작-------------"<<endl;
         for(int j = 1; j < 10; j++){
             cout << i << " x " << j <<" = "<< i*j << endl;
